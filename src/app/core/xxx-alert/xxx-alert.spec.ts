0d942f3bb32c20b71168f913a1f52a9a823cdbe1
angularexample <info@angularexample.com>
2025-09-22 20:40:13 -0400

    create xxx-alert.spec.ts with 100% coverage

---
+++ src/app/core/xxx-alert/xxx-alert.spec.ts
@@ -0,0 +1,43 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TestBed } from '@angular/core/testing';
+import { XxxAlert } from './xxx-alert';
+
+let service: XxxAlert;
+
+describe('XxxAlert', () => {
+  const expectedMessage: string = 'Expected message';
+  const mockMatSnackBar = {
+    open: jest.fn(),
+  };
+
+  TestBed.configureTestingModule({
+    providers: [
+      XxxAlert,
+      { provide: MatSnackBar, useValue: mockMatSnackBar },
+    ],
+  });
+
+  service = TestBed.inject(XxxAlert);
+
+  it('should be created', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should run showError', async () => {
+    service.showError(expectedMessage);
+    expect(mockMatSnackBar.open).toHaveBeenCalledWith(
+      expectedMessage, 'X', {panelClass: ['xxx-alert-error'], verticalPosition: 'top'});
+  });
+
+  it('should run showInfo', async () => {
+    service.showInfo(expectedMessage);
+    expect(mockMatSnackBar.open).toHaveBeenCalledWith(
+      expectedMessage, 'X', {duration: 5000, panelClass: ['xxx-alert-info'], verticalPosition: 'top'});
+  });
+
+  it('should run showWarning', async () => {
+    service.showWarning('Expected message');
+    expect(mockMatSnackBar.open).toHaveBeenCalledWith(
+      'Expected message', 'X', {duration: 10000, panelClass: ['xxx-alert-warning'], verticalPosition: 'top'});
+  });
+});
