9799d744f72b8b288b545c8335e7beedb83c53b2
angularexample <info@angularexample.com>
2025-09-24 18:25:34 -0400

    Add xxx-post-facade.spec.ts with 100% coverage

---
+++ src/app/features/xxx-post/xxx-post-facade.spec.ts
@@ -0,0 +1,106 @@
+import { mockPost } from './xxx-post.mocks';
+import { TestBed } from '@angular/core/testing';
+import { XxxPostFacade } from './xxx-post-facade';
+import { XxxPostStore } from './xxx-post-store';
+
+describe('XxxPostFacade', () => {
+  const mockXxxPostStore = {
+    isNoSelectedPost: jest.fn(),
+    isNoSelectedUser: jest.fn(),
+    isPostsEmpty: jest.fn(),
+    isPostsLoaded: jest.fn(),
+    isPostsLoading: jest.fn(),
+    isSaveButtonDisabled: jest.fn(),
+    posts: jest.fn(),
+    selectedPost: jest.fn(),
+    selectedPostId: jest.fn(),
+    selectedUserId: jest.fn(),
+    setSelectedPost: jest.fn(),
+    setPostForm: jest.fn(),
+    showPosts: jest.fn(),
+    updatePost: jest.fn(),
+  };
+  let service: XxxPostFacade;
+
+  TestBed.configureTestingModule({
+    providers: [
+      XxxPostFacade,
+      {provide: XxxPostStore, useValue: mockXxxPostStore},
+    ],
+  });
+
+  service = TestBed.inject(XxxPostFacade);
+  describe('constructor phase', () => {
+    it('should be created', () => {
+      expect(service).toBeDefined();
+    });
+
+    it('should have isNoSelectedPost', () => {
+      expect(service.isNoSelectedPost).toBeDefined();
+    });
+
+    it('should have isNoSelectedUser', () => {
+      expect(service.isNoSelectedUser).toBeDefined();
+    });
+
+    it('should have isPostsEmpty', () => {
+      expect(service.isPostsEmpty).toBeDefined();
+    });
+
+    it('should have isPostsLoaded', () => {
+      expect(service.isPostsEmpty).toBeDefined();
+    });
+
+    it('should have isPostsLoading', () => {
+      expect(service.isPostsLoading).toBeDefined();
+    });
+
+    it('should have isSaveButtonDisabled', () => {
+      expect(service.isSaveButtonDisabled).toBeDefined();
+    });
+
+    it('should have posts', () => {
+      expect(service.posts).toBeDefined();
+    });
+
+    it('should have selectedPost', () => {
+      expect(service.selectedPost).toBeDefined();
+    });
+
+    it('should have selectedPostId', () => {
+      expect(service.selectedPostId).toBeDefined();
+    });
+
+    it('should have selectedUserId', () => {
+      expect(service.selectedUserId).toBeDefined();
+    });
+  })
+
+  describe('setSelectedPost', () => {
+    it('should call postStore.setSelectedPost', () => {
+      service.setSelectedPost(mockPost.id);
+      expect(mockXxxPostStore.setSelectedPost).toHaveBeenCalledWith(mockPost.id);
+    });
+  });
+
+  describe('setPostForm', () => {
+    it('should call postStore.setPostForm', () => {
+      service.setPostForm(mockPost);
+      expect(mockXxxPostStore.setPostForm).toHaveBeenCalledWith(mockPost);
+    });
+  });
+
+  describe('showPosts', () => {
+    it('should call postStores.showPosts', () => {
+      service.showPosts();
+      expect(mockXxxPostStore.showPosts).toHaveBeenCalled();
+    });
+  });
+
+  describe('updatePost', () => {
+    it('should call postStores.updatePost', () => {
+      service.updatePost();
+      expect(mockXxxPostStore.updatePost).toHaveBeenCalled();
+    });
+  });
+});
