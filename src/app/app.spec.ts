af276da0822757be9fc3b90dcc4885c333f985cb
angularexample <info@angularexample.com>
2025-10-15 21:35:20 -0400

    improve app.spec.ts, 100% coverage

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,18 +1,46 @@
-import { provideHttpClient } from '@angular/common/http';
-import { provideZonelessChangeDetection } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { Component, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { TestBed } from '@angular/core/testing';
+import { XxxHeader } from './shared/xxx-header/xxx-header';
+import { XxxLoading } from './core/xxx-loading/xxx-loading';
+
+// Mock the child components
+@Component({
+  selector: 'xxx-header', // Same selector as the real child component
+  template: ''
+})
+class MockXxxHeader {
+}
+
+@Component({
+  selector: 'xxx-loading', // Same selector as the real child component
+  template: ''
+})
+class MockXxxLoading {
+}
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
       providers: [
-        provideHttpClient(),
         provideRouter([]),
         provideZonelessChangeDetection()
       ]
+    }).overrideComponent(App, {
+      remove: {
+        imports: [
+          XxxHeader,
+          XxxLoading
+        ]
+      },
+      add: {
+        imports: [
+          MockXxxHeader,
+          MockXxxLoading
+        ]
+      }
     }).compileComponents();
   });
 
